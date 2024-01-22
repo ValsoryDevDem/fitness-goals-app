19990c49e464790c2b554b9949403b92940323e7
ValsoryDevDem <116357364+ValsoryDevDem@users.noreply.github.com>
2024-01-22 09:07:55 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
 import Header from './components/Header';
 import Page1 from "./components/Page1";
